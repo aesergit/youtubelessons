430bf3ec4d2c395c9d1ac3cfd53af9a25310079c
aesergit <46317320+aesergit@users.noreply.github.com>
2022-09-03 15:34:16 +0300

    first commit

---
+++ Ders_2_Dosya_islemleri/Dosyalama/Dosyalama/Arayüz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dosyalama
+{
+    public partial class Arayüz : Form
+    {
+        public Arayüz()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
